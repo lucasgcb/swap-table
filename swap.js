f0aa7f1dacc6b3f4bc592fe33689fc0bd97c33f7
Lucas Gabriel Coliado Bandeira <lucascoliado@hotmail.com>
2022-12-05 20:53:20 -0300

    Add initial files

---
+++ swap.js
@@ -0,0 +1,9 @@
+function swap(){
+    input = document.getElementById('input-box')
+    output = document.getElementById('output-box')
+    output.value = input.value
+}
+
+swap = Document.getElementById('swap')
+
+swap.addEventListener("click", swap);
