23dc57b7040adb89bf571dd828815b9760bb7ea4
Lucas Gabriel Coliado Bandeira <lucascoliado@hotmail.com>
2022-12-05 21:10:43 -0300

    Add table

--- swap.js
+++ swap.js
@@ -1,9 +1,33 @@
 function swap(){
-    input = document.getElementById('input-box')
-    output = document.getElementById('output-box')
-    output.value = input.value
+    let input = document.getElementById('input-box')
+    let output = document.getElementById('output-box')
+    output.value = input.value.replace()
 }
 
-swap = Document.getElementById('swap')
+function getSwapValues()
+{
+    let table = document.getElementById('swap-table');
+    let swapValues = []
+    for (var r = 0, n = table.rows.length; r < n; r++) {
+        for (var c = 0, m = table.rows[r].cells.length; c < m; c++) {
+            swapValues.push(table.rows[r].cells[c].innerHTML);
+        }
+    }
+    return swapValues
+}
 
+function insertSwapValue(){
+    let table = document.getElementById('swap-table');
+    let input = document.getElementById('from-value').value
+    let output = document.getElementById('to-value').value
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = input;
+    cell2.innerHTML = output;
+}
+
+insert = document.getElementById('insert');
+swap = document.getElementById('swap');
+insert.addEventListener('click', insert);
 swap.addEventListener("click", swap);
