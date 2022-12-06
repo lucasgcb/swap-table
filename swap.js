e6483111cc0bfea4a74cc7f32bbc7deb785811fd
Lucas Gabriel Coliado Bandeira <lucascoliado@hotmail.com>
2022-12-06 00:20:00 -0300

    Add localstorage support

--- swap.js
+++ swap.js
@@ -1,37 +1,62 @@
-function swapValues(){
+function swapValues() {
     let input = document.getElementById('input-box')
     let output = document.getElementById('output-box')
-    let pairs = getSwapPairs()    
+    let pairs = getSwapPairs()
     output.value = input.value
-    pairs.forEach(function(swapValue) {
-      console.log(swapValue)
-      output.value = output.value.replaceAll(swapValue[0], swapValue[1])
+    pairs.forEach(function (swapValue) {
+        console.log(swapValue)
+        output.value = output.value.replaceAll(swapValue[0], swapValue[1])
     })
 }
 
-function removePair(rowId)
-{
+
+function setTable() {
+    let pairs = getSwapPairs()
+    localStorage.setItem('table', pairs)
+}
+
+
+
+function getTable() {
+    pairs = localStorage.getItem('table')
+    insertStoredTable(pairs)
+}
+
+function removePair(rowId) {
     var row = document.getElementById(rowId);
     row.parentNode.removeChild(row);
 }
 
-function getSwapPairs()
-{
+function getSwapPairs() {
     let table = document.getElementById('swap-table');
     let swapPairs = []
     for (var r = 0, n = table.rows.length; r < n; r++) {
         let fromValue = table.rows[r].cells[0].innerHTML
         let toValue = table.rows[r].cells[1].innerHTML
-        swapPair = [fromValue,toValue]
+        swapPair = [fromValue, toValue]
         swapPairs.push(swapPair);
     }
     return swapPairs
 }
 
-function insertSwapValue(){
+function insertStoredTable(pairs) {
+    removePair('row-0')
+    let table = document.getElementById('swap-table');
+    pairs.forEach(function (pair) {
+        let input = pair[0]
+        let output = pair[1]
+        insertRow(input, output, table)
+    })
+}
+
+function insertSwapValue() {
     let table = document.getElementById('swap-table');
     let input = document.getElementById('from-value').value
     let output = document.getElementById('to-value').value
+    insertRow(input, output, table)
+}
+
+function insertRow(input, output, table) {
     let row = table.insertRow(0);
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
@@ -46,10 +71,13 @@ function insertSwapValue(){
     cell3.innerHTML = '<button onclick=removePair(' + row.id + ') class=' + buttonClass + '>Delete</button>'
 }
 
-
+saveButton = document.getElementById('save-button');
+loadButton = document.getElementById('load-button');
 removeButton = document.getElementById('example-remove');
 insertButton = document.getElementById('insert-button');
 swapButton = document.getElementById('swap-button');
 insertButton.addEventListener('click', insertSwapValue);
-removeButton.addEventListener('click', function() {removePair('row-0')})
+removeButton.addEventListener('click', function () { removePair('row-0') })
 swapButton.addEventListener("click", swapValues);
+saveButton.addEventListener("click", setTable);
+loadButton.addEventListener("click", getTable);
